2c9cce7ae7b57679b97c2e49391b70841d5fa443
Iskandar <xsky.alexx@gmail.com>
2018-04-17 19:37:19 +0300

    add type annotation

--- src/helpers/redusers/index.js
+++ src/helpers/redusers/index.js
@@ -1,31 +1,38 @@
 //@flow
 type mainStateType = {
-	error:boolean,
-	data:Array<Object>
+	error: boolean,
+	data: Array < Object >
 };
 const MainInitState = {
-    error:false,
-    data:[]
+	error: false,
+	data: []
 };
 export function list(
-		state:mainStateType=MainInitState, 
-		action:{
-			state:mainStateType,
-			type:string
-		}
-	){
+	state: mainStateType = MainInitState,
+	action: {
+		state: mainStateType,
+		type: string
+	}
+) {
 	switch (action.type) {
 		case 'SEARCH_DATA':
-			return Object.assign({},state, action.state);
+			return Object.assign({}, state, action.state);
 		default:
 			return state;
 	}
 }
-
-export function showResults(state={show:false},action){
+type resultStateType = {
+	show: boolean
+}
+export function showResults(state: resultStateType = {
+	show: false
+}, action: {
+	state: resultStateType,
+	type: string
+}) {
 	switch (action.type) {
 		case 'SHOW_RESULTS':
-			return Object.assign({},state, action.state);
+			return Object.assign({}, state, action.state);
 		default:
 			return state;
 	}
